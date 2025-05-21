fb9e354898869b51ffa122334c743bcd1d37b131
Daniel321W <daniel.kozak@studenci.collegiumwitelona.pl>
2025-05-22 00:24:49 +0200

    docker and dpc manager

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Carbon\Carbon;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/now', function () {
+    return Carbon::now()->toDateTimeString();
 });
